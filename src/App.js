f12f8738053b0c37fbfa34f697996c0652d4d2e8
Ricardo Ramos <indricardoramos@gmail.com>
2022-09-13 19:47:35 -0300

    Fix: Frontend mentor semantic sugestions

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Balance } from './components/Balance/Balance';
 
 function App() {
   return (
-    <div className="App">
-     <Balance/>
-     <Chart/>
-    </div>
+    <main>
+      <div className="App">
+      <Balance/>
+      <Chart/>
+      </div>
+    </main>
   );
 }
 
